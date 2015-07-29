4223e73476b3ce3abbe8cf12b2d4b2cb59c85db0
root <root@ch3.opennet.ch>
2015-07-29 23:41:17 +0000

    additional setup update

--- functions.php
+++ functions.php
@@ -26,7 +26,7 @@ function get_ip($type)
 
 function db_config() {
 
-    $config = get_entry('system_config','ecallmgr');
+    $config = get_entry('system_config','crossbar.devices');
     if($config['err']) do_log("Get config Error:".$config['err']);
     else do_log("Get config Success:".$config['res']->_id,'v', __FILE__ ,__FUNCTION__, __LINE__);
 
@@ -155,6 +155,87 @@ function check_http($url, $timeout) {
 
 return($result);
 
+}
+
+/* do db->file (db=system_config, dir=./DB_BACKUP/) */
+function backup($db_a, $dir, $match=false)
+{
+    // get_all_docs
+    $dbs = get_entry($db_a , '/_all_docs');
+    $dbs = json_decode(json_encode($dbs['res']->rows), true);
+    @mkdir($dir, 0777, true);
+
+    foreach($dbs AS $k => $db){
+        /* backup only if match */
+        if($match == true && $match != $db['id']) continue;
+        $data = get_entry($db_a , "/".urlencode($db['id']));
+        file_put_contents($dir.urlencode($db['id']),json_encode($data['res']));
+    }
+    return("backup db->file finished\n");
+}
+
+/* do file->db (db=system_config, dir=./DB_BACKUP/) type=update or restore[none]*/
+function restore($db_a, $dir, $type=false)
+{
+    global $sag;
+
+    if ($handle = opendir($dir)) {
+        try {$sag->createDatabase($db_a);} catch(Exception $e) {echo $e->getMessage()."DB:".$db_a."\n";}
+        $sag->setDatabase($db_a);
+        while (false !== ($entry = readdir($handle))) {
+            if(".." == $entry||"." == $entry) continue;
+            $obj = json_decode(file_get_contents($dir.$entry));
+            unset($obj->_rev);
+            try {
+                if(preg_match("/^_/",urldecode($entry))) echo $sag->put(urldecode($entry), $obj)->body->ok;
+                else echo $sag->put($entry, $obj)->body->ok;
+            }
+            catch(Exception $e) {
+                if($type == 'update') {
+                    $now = get_entry($db_a , "/".$entry);
+                    if($now['res']->_rev) {
+                        $obj->_rev = $now['res']->_rev;
+                        $obj->views++;
+                    }
+                }
+                try {
+                    if(preg_match("/^_/",urldecode($entry))) echo $sag->put(urldecode($entry), $obj)->body->ok;
+                    else echo $sag->put($entry, $obj)->body->ok;
+                }
+                catch(Exception $e) {
+                    echo $e->getMessage()."DB:".$db_a." file:".urlencode($entry)."\n";
+                }
+            }
+        }
+    }
+    return("restore file->db finished\n");
+}
+
+function get_dbhost($hosts)
+{
+    global $testsleep;
+
+    while($host == false) {
+        foreach(explode(" ",$hosts) AS $testhost) { $host = check_couchdb($testhost); if($host) break;}
+        if($host) continue;
+        show_debug("No connect to cluster-db sleep now for next tray in (s):".$testsleep);
+        sleep($testsleep);
+    }
+return($host);
+
+}
+
+function merge_togetaher($object1, $object2, $typ)
+{
+    switch($typ) {
+        case 'json':
+            $res = json_encode(array_merge_recursive(json_decode( $object1, true ) , json_decode( $object2, true )));
+        break;
+        case 'object':
+            $res = array_merge_recursive($object1 , $object2);
+        break;
+    }
+    return($res);
 }
 
 ?>
---
+++ setup.php
@@ -0,0 +1,23 @@
+#!/usr/bin/php
+<?php
+
+require_once('config.php');
+require_once('phplib/Sag.php');
+require_once('functions.php');
+
+$host = get_dbhost($hosts);
+$sag = new Sag($host);
+
+function input()
+{
+    $stdin = fopen('php://stdin', 'r');
+    $resp = fgetc($stdin);
+
+return($resp);
+}
+echo "HAVE YOU set config in hosts? !!!!!\n";
+echo "system_config/crossbar.devices install file -> couchdb on $hosts (only 1)? Y/N ";$resp = input();if($resp == 'Y') restore('system_config', 'DB_INSTALL/system_config/', 'update');
+echo "system_config/crossbar.devices restore from couchdb -> file ? Y/N ";$resp = input();if($resp == 'Y') backup('system_config', 'DB_INSTALL/system_config/', 'crossbar.devices');
+sleep(2);
+
+?>
